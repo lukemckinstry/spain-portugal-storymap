19775670fcf4f720abeaa012a163f92d6b1d143e
Luke McKinstry <lukemckinstry@gmail.com>
2020-02-10 14:20:30 -0500

    Updates

---
+++ precache-manifest.f1fe78ea20baad4b80bbe2ca4452266e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4289511b84ec816909bf60a261d5d9d",
+    "url": "/spain-portugal-storymap/index.html"
+  },
+  {
+    "revision": "88e088bf502771c4c373",
+    "url": "/spain-portugal-storymap/static/css/main.de63c54f.chunk.css"
+  },
+  {
+    "revision": "457d57303c61647c3191",
+    "url": "/spain-portugal-storymap/static/js/2.3172dd64.chunk.js"
+  },
+  {
+    "revision": "88e088bf502771c4c373",
+    "url": "/spain-portugal-storymap/static/js/main.fac3526d.chunk.js"
+  },
+  {
+    "revision": "75f3d38b00df445e26da",
+    "url": "/spain-portugal-storymap/static/js/runtime~main.19159c53.js"
+  }
+]);
